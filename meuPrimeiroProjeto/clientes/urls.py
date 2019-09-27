b0bf02a078a2ffa5ae5931d8929350a2c0e8e8aa
Anderson Aranha <anderson.aranha@autovist.com.br>
2019-09-27 17:54:10 -0300

    update do crud

--- meuPrimeiroProjeto/clientes/urls.py
+++ meuPrimeiroProjeto/clientes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import persons_list
 from .views import persons_new
+from .views import persons_update
 
 
 urlpatterns = [
+    path('update/<int:id>/', persons_update, name='person_update'),
     path('new/', persons_new, name='person_new'),
     path('list/', persons_list, name='person_list'),
 ]
--- meuPrimeiroProjeto/clientes/views.py
+++ meuPrimeiroProjeto/clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 
@@ -7,8 +7,21 @@ def persons_list(request):
     return render(request, 'person.html', {'persons': persons})
 
 def persons_new(request):
-    form = PersonForm(request.POST, None)
+    form = PersonForm(request.POST or request.FILES or None)
 
     if form.is_valid():
         form.save()
+        return redirect('person_list')
     return render(request, 'person_form.html', {'form': form})
+
+
+def persons_update(request, id):
+    persons = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=persons)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
+
+
